4ab9d5e8985e6ff8c9ae0f48cad39e4f8bbea329
Justin <jburmesch.msp@gmail.com>
2020-06-12 23:46:28 +0900

    Redid DB Models - Incomplete

--- zcal/models.py
+++ zcal/models.py
@@ -7,12 +7,6 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-association_table = db.Table('association', db.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('meeting_id', db.Integer, db.ForeignKey('meeting.id'))
-) 
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -21,21 +15,21 @@ class User(db.Model, UserMixin):
     utype = db.Column(db.String(10), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    meetings = db.relationship('Meeting', secondary=association_table, back_populates='students')
+    teacher_meetings = db.relationship('Meeting', backref='teacher', lazy=True, foreign_keys= 'Meeting.teacher_id')
+    student_meetings = db.relationship('Meeting', backref='student', lazy=True, foreign_keys= 'Meeting.student_id')
+    
 
     def __repr__(self):
-        return f"User('{self.first}', '{self.last}', '{self.utype}', '{self.email}')"
+        return f"<User> {self.first} {self.last}, {self.utype}, {self.email})"
 
 
 class Meeting(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    meeting_id = db.Column(db.BigInteger, nullable=False, unique=True)
-    start_time = db.Column(db.DateTime, nullable=False)
-    teacher = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    students = db.relationship('User', secondary=association_table, back_populates="meetings")
+    teacher_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_time = db.Column(db.DateTime, nullable=False)
+    
 
     def __repr__(self):
-        return f"Meeting('{self.title}','{self.start_time.date}','{self.start_time.time})"
-  
+        return f"<Meeting> Date: {self.date_time.date}, Time: {self.date_time.time}, Teacher: {self.teacher.first} {self.teacher.last}, Student: {self.student.first} {self.student.last})"
