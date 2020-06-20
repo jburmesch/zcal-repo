0e923f94847bb80a06c7875f84898ab549fc3b6e
Justin <jburmesch.msp@gmail.com>
2020-06-20 13:37:32 +0900

    fixed login manager route in __init__.py

--- zcal/__init__.py
+++ zcal/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
 
 
