1ebcb9c27afcc564013a52191f4ce777deb66e8d
Justin <jburmesch.msp@gmail.com>
2020-07-18 15:49:21 +0900

    deleted a comment

--- zcal/__init__.py
+++ zcal/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# from zcal.config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
