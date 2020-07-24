c49c09a90fec6615a929ab56f2d17b4b39760f5c
Justin Burmesch <jburmesch.msp@gmail.com>
2020-07-25 08:51:21 +0900

    not much changed

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,8 @@
 import unittest
 from flask_testing import TestCase
-from zcal import create_app as create, db
+from zcalendar import create_app as create, db
 import datetime
-from zcal.models import (
+from zcalendar.zcal.models import (
     Course, User, Teacher, Schedule, Meeting, Student, Zoom, Timeslot
 )
 
@@ -181,60 +181,6 @@ class AuthTest(TestCase):
         ]
         return timeslots
     '''*********************'''
-
-    '''Helper Methods'''
-    def login(self, email, password):
-        return self.client.post('/auth/login', data=dict(
-            email=email,
-            password=password
-        ), follow_redirects=True)
-
-    def logout(self):
-        return self.get('/logout', follow_redirects=True)
-
-    def register(self, code, first, last, email, password,
-                 confirm_password):
-        return self.client.post('/auth/register', data=dict(
-            code=code,
-            first=first,
-            last=last,
-            email=email,
-            password=password,
-            confirm_password=confirm_password
-        ), follow_redirects=True)
-
-    def register_admin(self):
-        return self.register(
-            code='ADMIN',
-            first='Test',
-            last='Admin',
-            email='test@admin.com',
-            password='testpass',
-            confirm_password='testpass'
-        )
-
-    def login_admin(self):
-        return self.login(
-            email='test@admin.com',
-            password='testpass'
-        )
-
-    def register_student(self):
-        return self.register(
-            code='TEST',
-            first='Test',
-            last='Student',
-            email='test@student.com',
-            password='testpass',
-            confirm_password='testpass'
-        )
-
-    def login_student(self):
-        return self.login(
-            email='test@student.com',
-            password='testpass'
-        )
-
     '''Tests'''
     def test_db(self):
 
@@ -311,37 +257,6 @@ class AuthTest(TestCase):
         db.session.add(course)
         db.session.commit()
 
-    def test_main(self):
-        response = self.client.get("/", follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-        self.pr('Main', response)
 
-    def test_register(self):
-        course1 = Course(name='ADMIN', code='ADMIN')
-        course2 = Course(name='TEST', code='TEST')
-        db.session.add(course1)
-        db.session.add(course2)
-        db.session.commit()
-
-        response = self.register_admin()
-        self.assertEqual(response.status_code, 200)
-        self.pr('Reg Admin', response)
-
-        response = self.register_student()
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Account Created. Please check your '
-                      + b'email for account validation.', response.data)
-        self.pr('Reg Student', response)
-
-        nopass = dict(
-            code='TEST',
-            first='Test',
-            last='Student',
-            email='test@student.com',
-            password=None,
-            confirm_password=None
-        )
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
