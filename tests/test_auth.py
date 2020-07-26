ad60e6413a6b0f5ffba27f0f9ea7e44340901bdc
Justin Burmesch <jburmesch.msp@gmail.com>
2020-07-26 22:19:25 +0900

    added comments to test-auth and 8 char password check

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -84,20 +84,24 @@ class AuthTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_register(self):
+        # need courses for users to be created successfuly
         course1 = Course(name='ADMIN', code='ADMIN')
         course2 = Course(name='TEST', code='TEST')
         db.session.add(course1)
         db.session.add(course2)
         db.session.commit()
 
+        # ensure that the first user can register using the admin course
         response = self.register_admin()
         self.assertEqual(response.status_code, 200)
 
+        # ensure that a student can register successfuly
         response = self.register_student()
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Account Created. Please check your '
                       + b'email for account validation.', response.data)
 
+        # test non-matching passwords
         response = self.register(
             code='TEST',
             first='Test',
@@ -108,6 +112,7 @@ class AuthTest(TestCase):
         )
         self.assertIn(b'Field must be equal to password.', response.data)
 
+        # test bad email address
         response = self.register(
             code='TEST',
             first='Test',
@@ -118,6 +123,7 @@ class AuthTest(TestCase):
         )
         self.assertIn(b'Invalid email address.', response.data)
 
+        # test non-existant course code
         response = self.register(
             code='x',
             first='Test',
@@ -128,6 +134,8 @@ class AuthTest(TestCase):
         )
         self.assertIn(b'Course code not found.', response.data)
 
+        # make sure that admin registration doesn't work after 
+        # first user is registered
         response = self.register(
             code='ADMIN',
             first='Test',
@@ -137,6 +145,23 @@ class AuthTest(TestCase):
             confirm_password="testpass"
         )
         self.assertIn(b'Course code not found.', response.data)
+
+        # test passwords that are too short
+        response = self.register(
+            code='TEST',
+            first='Test',
+            last='Student',
+            email='t@t.com',
+            password="t",
+            confirm_password="t"
+        )
+        self.assertIn(
+            b'Field must be at least 8 characters long.',
+            response.data
+        )
+
+    def test_login(self):
+        pass
 
 
 if __name__ == "__main__":
