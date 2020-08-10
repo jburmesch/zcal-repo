5158a41a559181a5d9e87375a119d9c88c96ed7c
Justin Burmesch <jburmesch.msp@gmail.com>
2020-08-10 21:11:01 +0900

    removed old stuff after merge.

--- testing - old/testconfig.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'dev'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///../testing/test.db'
-TESTING = True
-DEBUG = False
-WTF_CSRF_ENABLED = False
-OAUTH_ID = 'dVqFPnPR0uljNdieqnlPQ'
-OAUTH_SECRET = 'zSqdnIN8uPGIcYiUFDtqTxmQ7ZHQjKYG'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
