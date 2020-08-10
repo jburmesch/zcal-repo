19860ee52ec0d521793077266523825a95c2445b
Justin Burmesch <jburmesch.msp@gmail.com>
2020-08-10 23:55:26 +0900

    started cleaning up admin.py, and fixed bugs in testing.

--- zcal/admin/admin.py
+++ zcal/admin/admin.py
@@ -1,4 +1,4 @@
-from flask import (render_template, redirect, url_for, request,
+from flask import (render_template, redirect, url_for,
                    flash, Blueprint)
 from zcal import db, bcrypt
 from zcal.admin.admin_forms import (
@@ -17,38 +17,43 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 @admin.route('/add-teacher', methods=['GET', 'POST'])
 @login_required
 def add_teacher():
+    # Ensure that user is Admin
     if current_user.utype == "Admin":
         form = TeacherForm()
         # see if form was submitted
-        if request.method == 'POST':
-            # check for valid data
-            if form.validate_on_submit():
-                password = secrets.token_urlsafe(6)
-                hashed_password = bcrypt.generate_password_hash(
-                    password
-                ).decode('utf-8')
-                user = User(
-                    first=form.first.data,
-                    last=form.last.data,
-                    utype="Teacher",
-                    email=form.email.data,
-                    password=hashed_password
-                )
-                db.session.add(user)
-                db.session.commit()
-                teacher = Teacher(
-                    user_id=user.id,
-                )
-                db.session.add(teacher)
-                db.session.commit()
-                # display success message
-                flash(f'Account Created. TEMP PASS: { password }', 'success')
-                return redirect(url_for('admin.add_teacher'))
+        if form.validate_on_submit():
+            password = secrets.token_urlsafe(6)
+            hashed_password = bcrypt.generate_password_hash(
+                password
+            ).decode('utf-8')
+
+            user = User(
+                first=form.first.data,
+                last=form.last.data,
+                utype="Teacher",
+                email=form.email.data,
+                password=hashed_password
+            )
+
+            db.session.add(user)
+            db.session.commit()
+
+            teacher = Teacher(
+                user_id=user.id,
+            )
+
+            db.session.add(teacher)
+            db.session.commit()
+
+            # display success message
+            flash(f'Account Created. TEMP PASS: { password }', 'success')
+            return redirect(url_for('admin.add_teacher'))
         return render_template(
             'add_teacher.html',
             title='Add Teacher',
             form=form
         )
+    # If not Admin:
     else:
         return redirect(url_for('cal'))
 
