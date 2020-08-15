3de709266c5fd9e57aaf63620f6182f4b7d0be00
Justin <jburmesch.msp@gmail.com>
2020-08-15 12:59:09 +0900

    removed testing folder from version control

--- testing/testconfig.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'dev'
-FLASK_ENV = 'development'
-WTF_CSRF_ENABLED = False
-OAUTH_ID = 'dVqFPnPR0uljNdieqnlPQ'
-OAUTH_SECRET = 'zSqdnIN8uPGIcYiUFDtqTxmQ7ZHQjKYG'
-SQLALCHEMY_DATABASE_URI = "sqlite:///../testing/test.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-TESTING = True
